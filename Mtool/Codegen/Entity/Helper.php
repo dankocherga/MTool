53b11d56355432210bcbaf67f705aa3b926d7c24
Daniel <dan.kocherga@gmail.com>
2011-01-24 12:52:44 +0300

    Helpers rewrite issue

--- Mtool/Codegen/Entity/Helper.php
+++ Mtool/Codegen/Entity/Helper.php
@@ -37,4 +37,28 @@ class Mtool_Codegen_Entity_Helper extends Mtool_Codegen_Entity_Abstract
 	 * @var string
 	 */
 	protected $_configNamespace = 'helpers';
+
+    /**
+	 * Rewrite Magento entity. Helpers have specific behavior - no alias in core configs.
+	 * 
+	 * @param string $originNamespace 
+	 * @param string $originPath 
+	 * @param string $path 
+	 * @param Mtool_Codegen_Entity_Module $module
+	 */
+	public function rewrite($originNamespace, $originPath, $path, Mtool_Codegen_Entity_Module $module)
+	{
+		// Create own class
+		$originPathSteps = $this->_ucPath(explode('_', $originPath));
+		$originModuleName = ucfirst($originNamespace);
+		$originClassName = implode('_', $originPathSteps);
+		$params = array(
+			'original_class_name' => "Mage_{$originModuleName}_{$this->_entityName}_{$originClassName}"
+		);
+		$className = $this->createClass($path, $this->_rewriteTemplate, $module, $params);
+
+		// Register rewrite in config
+		$config = new Mtool_Codegen_Config($module->getConfigPath('config.xml'));
+		$config->set("global/{$this->_configNamespace}/{$originNamespace}/rewrite/{$originPath}", $className);
+	}
 }
